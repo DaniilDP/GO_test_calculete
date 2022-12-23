c6193e452c4eb7c4d6f87f616c01bb7179a3794f
DANIIL PODKOPAEV <ya.logau@yandex.ru>
2022-12-23 03:50:14 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,74 +1,62 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
     "os"
+    "fmt"
+    "bufio"
     "strings"
     "strconv"
 )
 
 func int_to_rim(num int) string {
-    if num < 1{
-        return "У римлян были другие взгляды на этот счет."
-    }
+    
+    if num < 1 {return "У римлян были другие взгляды на этот счет."}
+    
 	r := [][]string{
 		{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
 		{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
 		{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}}
 	n := []int{100, 10, 1}
 	result := ""
+	
 	for k, v := range n {
 		result += r[k][num/v]
 		num = num % v
 	}
+	
 	return result
 }
 
 func search(key string, arr []string) int {
+    
     for index, value := range arr {
        if value == key {
           return index
        }
     }
+    
     return -1
  }
 
 func calculeted(a int, b int, operation string, rim_or_not bool) string{
     
     if operation == "+"{
-        if rim_or_not {
-            return int_to_rim(a+b)
-        } else {
-            return strconv.Itoa(a+b)
-        }
+        if rim_or_not {return int_to_rim(a+b)} else {return strconv.Itoa(a+b)}
     }
     
     if operation == "-"{
-        if rim_or_not {
-            return int_to_rim(a-b)
-        } else {
-            return strconv.Itoa(a-b)
-        }
+        if rim_or_not {return int_to_rim(a-b)} else {return strconv.Itoa(a-b)}
     }
     
     if operation == "*"{
-        if rim_or_not {
-            return int_to_rim(a*b)
-        } else {
-            return strconv.Itoa(a*b)
-        }
+        if rim_or_not {return int_to_rim(a*b)} else {return strconv.Itoa(a*b)}
     }
     
     if operation == "/"{
-	   if rim_or_not {
-            return int_to_rim(a/b)
-        } else {
-            return strconv.Itoa(a/b)
-        }
-    } 
-    return fmt.Sprint("Знак: ", operation, " не распознан!!!") 
+	   if rim_or_not {return int_to_rim(a/b)} else {return strconv.Itoa(a/b)}
+    }
     
+    return fmt.Sprint("Знак: ", operation, " не распознан!!!") 
 }
 
 func main() {
